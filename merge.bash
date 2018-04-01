8b360859da6ad5f6f923d2e60ec338f802a9ab9f
Harsh Shandilya <harsh@prjkt.io>
2018-04-01 21:18:09 +0530

    scripts: Cleanup and re-organise merge script
    Change-Id: I9b32cc51b667dbe0d20c0dd15ddf604c45d48ac8
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- merge.bash
+++ merge.bash
@@ -1,47 +1,51 @@
-source=$(pwd)
-tag=android-8.0.0_r17
-xos_ver=XOS-8.0
-prefix=https://android.googlesource.com/platform/
-gerrit_url="ssh://%s@review.halogenos.org:29418/android_%s"
+# Variables
+SOURCE=$(pwd)
+SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+TAG=android-8.1.0_r18
+XOS_VER=XOS-8.1
+PREFIX=https://android.googlesource.com/platform/
+GERRIT_URL="ssh://%s@review.halogenos.org:29418/android_%s"
 
-while getopts “opu:” OPTION
-do
-     case $OPTION in
-         o)
-             PUSH=true
-             ONLY_PUSH=true
-             ;;
-         p)
-             PUSH=true
-             ;;
-         u)
-             GERRIT_USER=$OPTARG
-             ;;
-     esac
+while getopts “opu:t:” OPTION; do
+  case ${OPTION} in
+      o)
+        PUSH=true
+        ONLY_PUSH=true
+        ;;
+      p)
+        PUSH=true
+        ;;
+      u)
+        GERRIT_USER=${OPTARG}
+        ;;
+      t)
+        TAG=${OPTARG}
+        ;;
+  esac
 done
 
-for project in $(cat merges.txt); do
-  path="$project"
-  repo="$project"
-  IFS=':' read -r -a array <<< "$project"
+for PROJECT in $(cat ${SCRIPT_DIR}/merges.txt); do
+  PATH="${PROJECT}"
+  REPO="${PROJECT}"
+  IFS=':' read -r -a array <<< "${PROJECT}"
   if [ ${#array[@]} -ge 2 ]; then
-    path=${array[0]}
-    repo=${array[1]}
+    PATH=${array[0]}
+    REPO=${array[1]}
   fi
-  if [ -d $path ]; then
-    cd $path
-    if [[ ! $ONLY_PUSH ]]; then
+  if [ -d ${PATH} ]; then
+    cd ${PATH}
+    if [[ ! ${ONLY_PUSH} ]]; then
       git remote remove aosp 2>/dev/null
-      git remote add aosp $prefix$repo
-      git fetch XOS $xos_ver
-      git reset --hard XOS/$xos_ver
-      git pull aosp $tag
+      git remote add aosp ${PREFIX}${REPO}
+      git fetch XOS ${XOS_VER}
+      git reset --hard XOS/${XOS_VER}
+      git pull aosp ${TAG}
     fi
-    if [[ $PUSH && ! -z $GERRIT_USER ]]; then
+    if [[ ${PUSH} && ! -z ${GERRIT_USER} ]]; then
       git remote remove gerrit 2>/dev/null
-      git remote add gerrit $(printf $gerrit_url "$GERRIT_USER" $(echo $path | sed 's/\//_/g') )
-      git push gerrit HEAD:refs/heads/$xos_ver
+      git remote add gerrit $(printf ${GERRIT_URL} "${GERRIT_USER}" $(echo ${PATH} | sed 's/\//_/g'))
+      git push gerrit HEAD:refs/heads/${XOS_VER}
     fi
-    cd $source
+    cd ${SOURCE}
   fi
 done
